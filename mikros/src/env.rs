6954da10700bbe6cd1ce0605a095885cbe9dc307
Rodrigo Freitas <rsfreitas@users.noreply.github.com>
2025-01-09 09:32:50 -0300

    docs: move resources folder

--- mikros/src/env.rs
+++ mikros/src/env.rs
@@ -84,6 +84,7 @@ mod tests {
 
     fn assets_path() -> std::path::PathBuf {
         let mut p = std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        p.pop();
         p.push("resources/test");
         p
     }
